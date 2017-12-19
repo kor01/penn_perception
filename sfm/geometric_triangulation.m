780811097c709382733f0ce29773b1d1f926ae5e
chenpu01 <chenpu01@baidu.com>
2017-12-19 09:20:22 +0800

    use sym cell function

--- sfm/geometric_triangulation.m
+++ sfm/geometric_triangulation.m
@@ -1,30 +1,63 @@
-function [delta_fn] = geometric_triangulation(Rs, Cs, p2ds)
+function [p3d] = geometric_triangulation(Rs, Cs, p2ds, initial, epsilon)
+% solve geometric triangulation by levenberg_marquardt
+
+p3d = initial;
+
+res, jacob, cur_loss = estimate_jacob(Rs, Cs, p2ds, p3d);
+
+while true
+    delta_x = (jacob' * jacob) / (jacob * res);
+    p3d = p3d - delta_x;
+    res, jacob, new_loss = estimate_jacob(Rs, Cs, p2ds, p3d);
+    if abs(new_loss - cur_loss) < epsilon
+        break;
+    end
+    cur_loss = new_loss;
+end
+
+end
+
+function [res, jacob, total_loss] = estimate_jacob(Rs, Cs, p2ds, p3d)
+
+fn = geometric_res_loss_grad();
 
 sz = length(Rs);
-losses = sym(zeros(sz, 1));
-
-syms real x y z;
+residues = zeros(sz, 1);
+jacob = zeros(sz, 3);
+total_loss = 0;
 
 for i=1:1:sz
     R = Rs(i, :, :); C = Cs(i, :); p2d = p2ds(i, :);
-    proj = R * [x, y, z]' + C;
-    proj = proj / proj(3);
-    residue = proj - p2d;
-    losses(i) = sum(residue' * residue);
+    [res, loss, grad] = fn(p3d, p2d, R, C);
+    residues(i) = res;
+    total_loss = total_loss + loss;
+    jacob(i, :) = grad;
 end
 
-jacob = jacobian(losses, [x, y, z]);
-
 end
 
-function [grad, residue] = levenberg_marquardt(R, C, p2d, x, y, z)
+function [fn] = geometric_res_loss_grad()
 
-proj = R * [x y z]' + C;
+persistent ret;
 
-proj = proj / proj(3);
+if ~isempty(ret)
+    fn = ret;
+    return;
+end
+
+p3d = sym('p3d_%d', [3, 1], 'real');
+p2d = sym('p2d_%d', [2, 1], 'real');
+R = sym('R%d', [3, 3], 'real');
+C = sym('C%d', [3, 1], 'real');
+
+proj = R * p3d + C; proj = proj(1:2) /proj(3);
 
 residue = proj - p2d;
+loss = sum(residue' * residue);
+grad = gradient(loss, p3d);
 
-grad = gradient(residue, [x, y, z]);
+fn = matlabFunction(residue, loss, grad, 'Vars', {p3d, p2d, R, C});
+
+ret = fn;
 
 end
